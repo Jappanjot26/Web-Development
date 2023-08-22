dfc2d678c77848af49c07208346cd9540f09cebe
Jappanjot26 <singhjappanjot1@gmail.com>
2023-08-22 12:25:37 +0530

    Adding files

---
+++ 02-HTML-Fundamentals/class3.js
@@ -0,0 +1,17 @@
+function cube(x){
+  document.write("<br>Cube is ="+x*x*x);
+}
+function sum(x,y){
+  var d=x+y;
+  document.write("<br>Sum = "+d);
+
+}
+function Check_Even_Odd(x){
+  if(x%2==0){
+    document.write("<br>Number "+x+" is Even");
+
+  }
+  else{
+    document.write("<br>Number "+x+" is Odd");
+  }
+}
