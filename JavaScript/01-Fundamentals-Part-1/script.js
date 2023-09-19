8e1c7ff33c09d9fd3044549ba22c9edab95b2346
Jappanjot26 <singhjappanjot1@gmail.com>
2023-09-19 22:39:37 +0530

    Completed fundamentals part 1

--- JavaScript/01-Fundamentals-Part-1/script.js
+++ JavaScript/01-Fundamentals-Part-1/script.js
@@ -69,9 +69,9 @@ console.log(typeof null); //Bug --> shows object instead of null.
 // lastName = "Kalra";
 // console.log(lastName);
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
 // console.log(ageJonas, ageSarah);
 
 // console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
@@ -100,21 +100,177 @@ const ageSarah = now - 2018;
 // console.log(x, y);
 // const avgAge = (ageJonas + ageSarah) / 2;
 // console.log(ageSarah, ageJonas);
-const year = 2037;
-const firstName = "John";
-const job = "teacher";
-const birthYear = 1991;
+// const year = 2037;
+// const firstName = "John";
+// const job = "teacher";
+// const birthYear = 1991;
 
-const jonas =
-  "I'm " + firstName + ", a " + (year - birthYear) + "years old " + job + "!";
-console.log(jonas);
-const jonasNew = `I'm $(firstName), a $(year - birthYear) year old $(job)!`;
-console.log(jonasNew);
+// const jonas =
+//   "I'm " + firstName + ", a " + (year - birthYear) + "years old " + job + "!";
+// console.log(jonas);
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+// console.log(jonasNew);
 
-console.log("string with \n multiple \n lines");
-console.log(`string with
-multiple
-lines`);
+// console.log("string with \n multiple \n lines");
+// console.log(`string with
+// multiple
+// lines`);
+
+// const age = 15;
+// if (age >= 18) {
+//   console.log("Sarah can start driving");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too Young. Wait another ${yearsLeft} years`);
+// }
+
+/*
+//type conversion
+const inputYear = "1991";
+console.log(Number(inputYear) + 18);
+console.log(inputYear + 18); //original str doesn't change
+
+console.log(Number("Jonas")); //NaN -- Not a Number (or Invalid Number)
+
+console.log(typeof NaN);
+console.log(String(23), 23);
+
+//type coersion (+ --> converts number to string)
+console.log("I am " + 18 + " years old");
+// -,*,/ --> converts str in num
+console.log("23" - "10" - 3);
+console.log(("20" / "10") * 3);
+
+//Guess the output
+let n = "1" + 1;
+n = n - 1;
+console.log(n); //10
+*/
+
+/*
+//5 falsy values : 0,'',undefined,null,NaN
+//other all are truthy values
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean("Jonas"));
+console.log(Boolean(""));
+console.log(Boolean({}));
+console.log(Boolean(NaN));
+*/
+/*
+//type coersion by if-else
+const money = 1000;
+if (money) {
+  console.log("Don't spend it at all ;)");
+} else {
+  console.log("You should get a job!");
+}
+//truthy and falsy to check if var is defined or not
+let height;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
+*/
+
+/*
+//equlaity operators
+//strict - === (no type-coersion)
+//loose - == (does type-coersion)
+const age = 18;
+if (age === 18) console.log("You just became an adult :D");
+if (20 == "20") console.log("You just became an adult :D(loose)");
+if (20 === "20") console.log("You just became an adult :D(strict)");//doesn't log this string
+
+//promt is used to take input from user
+const fav = prompt("Whats your fav number ?");
+console.log(fav); //store in string
+console.log(typeof fav);
+if (fav === 23) {
+  //strict equality
+  console.log("Cool! 23 is an amazing number!");
+} else if (fav === 7) {
+  console.log("7 is also a Cool number!");
+} else {
+  console.log("Number is not 23 or 7");
+}
+//!=  -> loose
+//!== -> strict
+if(fav !== 23)console.log("Not equal to 23");
+*/
+
+//-------------------------------------------------------//
+/*
+const hasDriversLicense = true; //A
+const hasGoodVision = true; //B
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasGoodVision);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+// if (shouldDrive) {
+//   console.log("Sarah should drive");
+// } else {
+//   console.log("Someone else should drive");
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sarah should drive");
+} else {
+  console.log("Someone else should drive");
+}*/
+//-------------------------------------------------------
+/*
+const day = "monday";
+switch (day) {
+  case "monday":
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory vedios");
+    break;
+  case "wednesday":
+  case "thrusday":
+    console.log("write code examples");
+    break;
+  case "friday":
+    console.log("Record vedios");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid day!");
+}
+*/
+//-------------------------------------------------------
+
+const age = 15;
+//ternary operator
+age >= 18
+  ? console.log("i like to drink wine🍷")
+  : console.log("I like to drink water 🫗");
+
+// let drink;
+// if(age>=18){
+//   drink = 'wine'
+// } else {
+//   drink = 'water';
+// }
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+
+//-------------------------------------------------------
+//
+//
+//
 /*
 //  Assignment - Value and variables
 // let country = "India";
