8cadb3866d53ef6ca341d51f5e2317fedc6e6c3c
Jappanjot26 <singhjappanjot1@gmail.com>
2024-09-23 12:12:31 +0530

    Making Promises to run parallely using Promise.all()

--- JavaScript/16-Asynchronous/script.js
+++ JavaScript/16-Asynchronous/script.js
@@ -568,3 +568,24 @@ console.log('Loading...');
   console.log('Finished...');
 })();
 */
+
+const getCapitals = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+    const [[data1], [data2], [data3]] = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+
+    console.log(data1.capital[0], data2.capital[0], data3.capital[0]);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getCapitals('india', 'australia', 'usa');
