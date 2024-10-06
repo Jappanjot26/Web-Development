0d2d7eda6b621abc3bb580b99bbf5a2861e74031
Jappanjot26 <singhjappanjot1@gmail.com>
2024-10-06 23:24:44 +0530

    Handling Events

---
+++ React/04-steps/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    setStep(step > 1 ? step - 1 : step);
+  }
+  function handleNext() {
+    setStep(step < messages.length ? step + 1 : step);
+  }
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <div className="message">
+        Step {step}: {messages[step - 1]}
+      </div>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
