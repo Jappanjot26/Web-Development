0cae184bd18bc69f356fb8f69fe15e3d339d31d2
Jappanjot26 <singhjappanjot1@gmail.com>
2024-10-10 23:41:09 +0530

    changing states with good practice

--- React/04-steps/src/App.js
+++ React/04-steps/src/App.js
@@ -8,32 +8,45 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  // const [test] = useState({ name: "Jonas" });
   const style = { backgroundColor: "#7950f2", color: "#fff" };
 
   function handlePrevious() {
-    setStep(step > 1 ? step - 1 : step);
+    if (step > 1) setStep((s) => s - 1);
   }
   function handleNext() {
-    setStep(step < messages.length ? step + 1 : step);
+    if (step < messages.length) setStep((s) => s + 1);
+    // BAD PRACTICE
+    // Consider states as immutable, mutate with react tools only
+    // test.name = "Fred";
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
-      <div className="message">
-        Step {step}: {messages[step - 1]}
-      </div>
-      <div className="buttons">
-        <button style={style} onClick={handlePrevious}>
-          Previous
-        </button>
-        <button style={style} onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <div className="message">
+            Step {step}: {messages[step - 1]}
+            {/* test.name */}
+          </div>
+          <div className="buttons">
+            <button style={style} onClick={handlePrevious}>
+              Previous
+            </button>
+            <button style={style} onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
