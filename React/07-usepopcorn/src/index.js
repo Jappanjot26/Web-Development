6d54d455ded9694dd34fb0c0464f3e779e8687ef
Jappanjot26 <singhjappanjot1@gmail.com>
2024-11-10 23:43:50 +0530

    Building a Reusable Star Rating Component

---
+++ React/07-usepopcorn/src/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [rating, setRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setRating} />
+      <p>Movie Rating : {rating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Average", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={10}
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
+    <StarRating />
+    <Test />
+  </React.StrictMode>
+);
